18dfe3f14ebfa115da85fad5e008c7607ae07356
neoatlantis <rijndael@yeah.net>
2015-05-01 00:44:50 +0200

    add list

--- sys/storage.js
+++ sys/storage.js
@@ -3,17 +3,21 @@ function storage(e){
 
     var fs = require('fs'),
         buffer = require('buffer'),
-        path = require('path');
+        path = require('path'),
+        crypto = require('../lib/crypto.js');
 
     function getPath(filename){
         return path.resolve(e.config.storage, filename);
     };
 
+    function testFilename(filename){
+        return /^(kv|(list_[0-9a-z]+))_[0-9a-zA-Z\.\-]+$/.test(filename);
+    };
+
     function save(key, data, callback){
         if(3 != arguments.length)
             return callback('Invalid storage arguments count.');
-        if(!/^(kv|list)_[0-9a-zA-Z\.\-]+$/.test(key))
-            return callback('Storage key invalid.');
+        if(!testFilename(key)) return callback('Storage key invalid.');
         var filename = getPath(key),
             filedata = new buffer.Buffer(JSON.stringify(data), 'utf-8');
         fs.writeFile(filename, filedata, callback);
@@ -23,8 +27,7 @@ function storage(e){
     function read(key, callback){
         if(2 != arguments.length)
             return callback('Invalid storage arguments count.');
-        if(!/^(kv|list)_[0-9a-zA-Z\.\-]+$/.test(key))
-            return callback('Storage key invalid.');
+        if(!testFilename(key)) return callback('Storage key invalid.');
         var filename = getPath(key);
         fs.readFile(filename, function(err, data){
             if(err){
@@ -47,6 +50,34 @@ function storage(e){
     };
     this.kv.get = function(key, callback){
         read('kv_' + key, callback);
+    };
+
+
+    this.list = function(name){
+        var ret = {};
+        ret.all = function(callback){
+            fs.readdir(getPath('.'), function(err, files){
+                if(err) return callback('Storage reading list failure.');
+                var filter = [], prefix = 'list_' + name + '_';
+                for(var i in files){
+                    if(!testFilename(files[i])) continue;
+                    if(prefix == files[i].substr(0, prefix.length)){
+                        filter.push(files[i].substr(prefix.length));
+                    };
+                };
+                filter.sort();
+                callback(null, filter);
+            });
+        };
+        ret.set = function(data, callback){
+            if(!callback) callback = function(){};
+            var key = crypto.util.uuid();
+            save('list_' + name + '_' + key, data, callback);
+        };
+        ret.get = function(key, callback){
+            read('list_' + name + '_' + key, callback);
+        };
+        return ret;
     };
 
     return this;
